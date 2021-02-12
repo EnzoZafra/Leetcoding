449d7376fd0effc253ecae3728b8c8c5a72e6909
Enzo Zafra <zafra@ualberta.ca>
2021-02-12 14:47:29 -0700

    Time: 1396 ms (5.64%), Space: 22.8 MB (10.36%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,17 +4,25 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        maxPriceRight = [0 for x in range(len(prices))]
-        maxPriceRight[-1] = prices[-1]
+        if not prices:
+            return 0
+        
+        # store the max price up to i
+        max_right_scan = [-1 for _ in range(len(prices))]
+        
+        max_right_scan[-1] = prices[-1]
         for i in range(len(prices)-2, -1, -1):
-            maxPriceRight[i] = max(maxPriceRight[i+1], prices[i])
+            price = prices[i]
+            max_right_scan[i] = max(price, max_right_scan[i+1])
+        
+        print(max_right_scan)
         
         maxProfit = 0
-        minPriceSoFar = prices[0]
-        for i in range(1, len(prices)):
+        minBuyPrice = float('inf')
+        for i in range(len(prices)):
             price = prices[i]
-            minPriceSoFar = min(minPriceSoFar, price)
-            maxProfit = max(maxProfit, maxPriceRight[i] - minPriceSoFar)
+            minBuyPrice = min(minBuyPrice, price)
+            maxProfit = max(maxProfit, max_right_scan[i] - minBuyPrice)
         
         return maxProfit
-            
+        
