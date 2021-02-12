79056ab71421bbc810eeed3681ad9132c0784e08
Enzo Zafra <zafra@ualberta.ca>
2021-02-12 13:39:57 -0700

    Time: 16 ms (81.14%), Space: 13.5 MB (63.64%) - LeetHub

--- robot-bounded-in-circle/robot-bounded-in-circle.py
+++ robot-bounded-in-circle/robot-bounded-in-circle.py
@@ -4,33 +4,22 @@ class Solution(object):
         :type instructions: str
         :rtype: bool
         """
-        # the robot is in a circle iff it changes direction or it goes back to zero
+        curr = [0,0]
         
-        curr = [0, 0]
-        directions = ['N', 'E', 'S', 'W']        # clockwise order
-        forward_list = [(1,0), (0, 1), (-1, 0), (0, -1)]
-        curr_direction_idx = 0 
+        # north, east, south, west
+        forward_directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        curr_direction = 0
         
         for char in instructions:
             if char == 'G':
-                # move forward
-                movement = forward_list[curr_direction_idx]
+                movement = forward_directions[curr_direction]
                 curr[0] += movement[0]
                 curr[1] += movement[1]
-                 
             elif char == 'L':
-                # 90* left 
-                if curr_direction_idx == 0:
-                    curr_direction_idx = 3
-                else:
-                    curr_direction_idx -= 1 
+                curr_direction -= 1
+                curr_direction = curr_direction % 4
             elif char == 'R':
-                # 90* right
-                if curr_direction_idx == 3:
-                    curr_direction_idx = 0
-                else:
-                    curr_direction_idx += 1
+                curr_direction += 1
+                curr_direction = curr_direction % 4
         
-        return curr == [0, 0] or curr_direction_idx != 0
-        
-        
+        return curr == [0, 0] or curr_direction != 0
