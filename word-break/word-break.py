8bccb454d6a9f977eb807f311b78214d3f904837
Enzo Zafra <zafra@ualberta.ca>
2021-02-12 17:11:37 -0700

    Time: 60 ms (7.53%), Space: 13.6 MB (57.31%) - LeetHub

---
+++ word-break/word-break.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        wordSet = set(wordDict)
+        
+        dp = [False for _ in range(len(s)+ 1)]
+        dp[0] = True
+        
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    print(dp)
+                    break
+                    
+        return dp[-1]
