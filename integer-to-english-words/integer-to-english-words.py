8f610f658e02d72826b0694226bced0358ce1c0e
Enzo Zafra <zafra@ualberta.ca>
2021-02-13 16:22:42 -0700

    Time: 28 ms (21.07%), Space: 13.2 MB (95.36%) - LeetHub

--- integer-to-english-words/integer-to-english-words.py
+++ integer-to-english-words/integer-to-english-words.py
@@ -1,110 +1,117 @@
 class Solution(object):
+    def convert_single_dig(self, num):
+        translator = {
+            1: 'One',
+            2: 'Two',
+            3: 'Three',
+            4: 'Four',
+            5: 'Five',
+            6: 'Six',
+            7: 'Seven',
+            8: 'Eight',
+            9: 'Nine'
+        }
+        
+        return translator.get(num, None)
+    
+    def convert_two_dig(self, num):
+        if num == 0:
+            return None
+        
+        if num < 10:
+            return self.convert_single_dig(num)
+        elif 10 <= num < 20:
+            return self.convert_two_dig_less_twenty(num)
+        else:
+            return self.convert_two_dig_greater_twenty(num)
+            
+        
+    def convert_two_dig_less_twenty(self, num):
+        translator = {
+            10: 'Ten',
+            11: 'Eleven',
+            12: 'Twelve',
+            13: 'Thirteen',
+            14: 'Fourteen',
+            15: 'Fifteen',
+            16: 'Sixteen',
+            17: 'Seventeen',
+            18: 'Eighteen',
+            19: 'Nineteen'
+        }
+        
+        return translator.get(num)
+
+    def convert_two_dig_greater_twenty(self, num):
+        string = str(num)
+        tens = int(string[0])
+        
+        translator = {
+            2: 'Twenty',
+            3: 'Thirty',
+            4: 'Forty',
+            5: 'Fifty',
+            6: 'Sixty',
+            7: 'Seventy',
+            8: 'Eighty',
+            9: 'Ninety'
+        }
+        tensString = translator.get(tens) 
+        singleString = self.convert_single_dig(int(string[1]))
+        
+        return tensString + " " + singleString if singleString else tensString
+    
+    def convert_three_dig(self, num):
+        if 99 >= num:
+            return self.convert_two_dig(num)
+        else:
+
+            string = str(num)
+            single = int(string[0])
+            double = int(string[1:])
+
+            singleString = self.convert_single_dig(single) + " " + "Hundred"
+            doubleString = self.convert_two_dig(double)
+
+            if not doubleString:
+                return singleString
+            elif not singleString:
+                return doubleString
+
+            return singleString + " " + doubleString
+        
+
     def numberToWords(self, num):
         """
         :type num: int
         :rtype: str
         """
-         
-        def convert_one_digit(num):
-            mapping = {
-                1: 'One',
-                2: 'Two',
-                3: 'Three',
-                4: 'Four',
-                5: 'Five',
-                6: 'Six',
-                7: 'Seven',
-                8: 'Eight',
-                9: 'Nine'
-            }
-            
-            return mapping.get(num)
+        out = ''
         
-        def convert_two_digit_less_twenty(num):
-            mapping = {
-                10: 'Ten',
-                11: 'Eleven',
-                12: 'Twelve',
-                13: 'Thirteen',
-                14: 'Fourteen',
-                15: 'Fifteen',
-                16: 'Sixteen',
-                17: 'Seventeen',
-                18: 'Eighteen',
-                19: "Nineteen"
-            }
-            
-            return mapping.get(num)
-        
-        def convert_tens(num):
-            mapping = {
-                2: 'Twenty',
-                3: 'Thirty',
-                4: 'Forty',
-                5: 'Fifty',
-                6: 'Sixty',
-                7: 'Seventy',
-                8: 'Eighty',
-                9: 'Ninety'
-            }
-            
-            return mapping.get(num)
-        
-        def convert_two_digit(num):
-            if not num:
-                return ''
-            elif num < 10:
-                return convert_one_digit(num)
-            elif num < 20:
-                return convert_two_digit_less_twenty(num)
-            else:
-                ten = num // 10
-                tenString = convert_tens(ten)
-                
-                rest = num - (ten * 10)
-                    
-                if rest:
-                    return tenString + ' ' + convert_one_digit(rest)
-                else:
-                    return tenString
-        
-        def convert_three_digit(num):
-            hundreds = num // 100
-            rest = num - (hundreds * 100)
-            
-            if hundreds and rest:
-                return convert_one_digit(hundreds) + " Hundred " + convert_two_digit(rest)
-            elif not hundreds and rest:
-                return convert_two_digit(rest)
-            elif hundreds and not rest:
-                return convert_one_digit(hundreds) + " Hundred"
-            
-        billion = num // 1000000000
-        million = (num - (billion * 1000000000)) // 1000000
-        thousand = (num - (billion * 1000000000) - (million * 1000000)) // 1000
-        rest = (num - (billion * 1000000000) - (million * 1000000) - (thousand * 1000))
-        
-        if not num:
+        if num == 0:
             return 'Zero'
+
+        billion = num // 1000000000
+        if billion > 0:
+            out += self.convert_three_dig(billion) + ' Billion'
+            num = num % 1000000000
         
-        result = ''
-        
-        if billion:
-            billString = convert_three_digit(billion)
-            result = billString + " Billion" 
-        
+        million = num // 1000000
         if million:
-            millString = convert_three_digit(million)
-            result += ' ' if result else ''
-            result += millString + " Million"
-            
+            out += ' ' if out else ''
+            out += self.convert_three_dig(million) + " Million"
+            num = num % 1000000
+        
+        thousand = num // 1000
         if thousand:
-            thousandString = convert_three_digit(thousand)
-            result += ' ' if result else ''
-            result += thousandString + " Thousand"
+            out += ' ' if out else ''
+            out += self.convert_three_dig(thousand) + " Thousand"
+            num = num % 1000    
         
-        if rest:
-            result += ' ' if result else ''
-            result += convert_three_digit(rest)
+        if num:
+            out += ' ' if out else ''
+            out += self.convert_three_dig(num)
+            
+        return out
+    
         
-        return result
