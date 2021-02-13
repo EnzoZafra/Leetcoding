3ba5364b5c9ea5e4acdbb95a96d433baabd0bef1
Enzo Zafra <zafra@ualberta.ca>
2021-02-13 14:36:39 -0700

    Time: 404 ms (27.15%), Space: 18.4 MB (15.63%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,43 +1,55 @@
 class Solution(object):
+        
+    def isInGrid(self, rowLen, colLen, dr, dc):
+        return 0 <= dr < rowLen and 0 <= dc < colLen
+        
     def exist(self, board, word):
         """
         :type board: List[List[str]]
         :type word: str
         :rtype: bool
         """
-        if not board:
-            return False
+        # bfs. only proceed to a child, if the value of the child is the next letter we want to visit.
+        # keep a visited set so since we can use the same cell more than once
         
-        self.R = len(board)
-        self.C = len(board[0])
-        self.directions = [(0,1), (1,0), (-1, 0), (0, -1)]
+        if not board or not word:
+            return None
 
-        def backtrack(row, col, path):
-            if len(path) == 0:
+        # we need to backtrack because sometimes, we should be taking a different way
+        self.moves = [(0,1), (1,0), (-1,0), (0,-1)]
+        self.rowLen = len(board)
+        self.colLen = len(board[0])
+        
+        def bfs(row, col, curr_word):
+            # base case, we've completed our word
+            if len(curr_word) == 0:
                 return True
             
-            # if it is not the correct letter or out of bounds, no need to proceed
-            if row < 0 or col < 0 or row >= self.R or col >= self.C or board[row][col] != path[0]:
-                return False
-            
-            # mark visited
-            board[row][col] = '#'
-            
-            for direction in self.directions:
-                dr = row + direction[0]
-                dc = col + direction[1]
-                
-                if backtrack(dr, dc, path[1:]):
-                    return True
-                
-            # backtrack the mark
-            board[row][col] = path[0]
-            
+            for move in self.moves:
+                dr = row + move[0]
+                dc = col + move[1]
+                # if the neighbor is not out of bounds and its the letter we need, take the move
+                if self.isInGrid(self.rowLen, self.colLen, dr, dc) and board[dr][dc] == curr_word[0] and (dr, dc) not in self.seen:
+                    self.seen.add((dr, dc))
+                    if bfs(dr, dc, curr_word[1:]):
+                        return True
+                    self.seen.remove((dr, dc))
+                    
             return False
-        
-        for row in range(self.R):
-            for col in range(self.C):
-                if backtrack(row, col, word):
-                    return True
-        
+                    
+       
+        for row in range(self.rowLen):
+            for col in range(self.colLen):
+                if board[row][col] == word[0]:
+                    self.seen = set()
+                    self.seen.add((row, col))
+                    if bfs(row,col,word[1:]):
+                        return True
+                    
         return False
+        
+    
+        
+        
+        
+        
