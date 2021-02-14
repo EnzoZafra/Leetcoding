e791757846fe54b068346b16cfe246acc435f114
Enzo Zafra <zafra@ualberta.ca>
2021-02-14 15:21:12 -0700

    Time: 72 ms (96.47%), Space: 26.1 MB (50.88%) - LeetHub

--- binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -5,31 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-        
     def maxPathSum(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-    
+
         def recur(node):
             if not node:
                 return 0
-
-            # leaf 
-
-            left = max(recur(node.left), 0)
-            right = max(recur(node.right), 0)
-
-            # we should either return the new tree, or old tree + extra path
-            newTree = node.val + left + right
-
-            self.max = max(self.max, newTree)
-
-            return node.val + max(left, right)
+           
+            leftSum = max(recur(node.left), 0)
+            rightSum = max(recur(node.right), 0)
+            
+            combined = leftSum + rightSum + node.val
+            self.maxPathSum = max(combined, self.maxPathSum)
+            
+            return node.val + max(leftSum, rightSum)
         
-        self.max = float('-inf')
+        self.maxPathSum = float('-inf')
         recur(root)
-        
-        return self.max
-        
+        return self.maxPathSum
