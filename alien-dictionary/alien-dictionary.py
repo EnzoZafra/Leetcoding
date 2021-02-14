b340b559ea44c061d3dc82194b86bf2dc9c136b0
Enzo Zafra <zafra@ualberta.ca>
2021-02-14 13:55:25 -0700

    Time: 20 ms (80.39%), Space: 13.7 MB (14.33%) - LeetHub

--- alien-dictionary/alien-dictionary.py
+++ alien-dictionary/alien-dictionary.py
@@ -1,56 +1,84 @@
 class Solution(object):
+    def find_first_diff_char(self, word1, word2):
+        
+        p1 = 0
+        p2 = 0
+        
+        while p1 < len(word1) and p2 < len(word2):
+            char1 = word1[p1] 
+            char2 = word2[p2]
+            
+            if char1 != char2:
+                return char1, char2
+            else:
+                p1 += 1
+                p2 += 1
+        
+        return None, None
+            
+    def build_graph(self, words):
+        if not words:
+            return None
+        
+        graph = collections.defaultdict(list)
+        prev_word = words[0]
+        for word in words[1:]:
+            char1, char2 = self.find_first_diff_char(prev_word, word)
+            if char1 and char2:
+                graph[char1].append(char2)
+            elif len(prev_word) > len(word):
+                # if there is no difference and the prev word is longer than the next word
+                return -1
+            
+            prev_word = word
+        return graph
+        
+    def get_zero_indegrees(self, nodes, graph):
+        indegrees = {node:0 for node in nodes}
+        
+        for node in graph:
+            for neighbor in graph[node]:
+                indegrees[neighbor] += 1
+        
+        zeroInDegrees = []
+        for node, indegree in indegrees.items():
+            if indegree == 0:
+                zeroInDegrees.append(node)
+                
+        return indegrees, zeroInDegrees
+    
     def alienOrder(self, words):
         """
         :type words: List[str]
         :rtype: str
         """
-        # char in [0] comes before char in [1]
-        graph = defaultdict(set)
-        inDegrees = {c:0 for word in words for c in word}
+        # for each pair of words, find the first index where the characters differ
+        # build graph character [before, after]
+        # wrt vs wrf - > t and f since wrt came before wtf, -> [t, f]
+        # [t, f], [w, e], [r, t], [e,r]
         
-        for word1, word2 in zip(words, words[1:]):
-            for char1, char2 in zip(word1, word2):
-                if char1 != char2:
-                    if char2 not in graph[char1]:
-                        graph[char1].add(char2)
-                        inDegrees[char2] += 1
-                    break
-            else:
-                # If there is no difference, make sure len2 is longer (not a prefix)
-                if len(word2) < len(word1):
-                    return ''
-                
-        print(graph)
-        print(inDegrees)
+        # we can do a topological sort to get a sorted list 
         
-        # topological sort
-        zeroInDegrees = deque()
-        for key,value in inDegrees.items():
-            if value == 0:
-                zeroInDegrees.append(key)
-        
-        print(zeroInDegrees)
-        out = []
-        
-        # zeroInDegrees is a queue now 
-        while zeroInDegrees:
-            curr = zeroInDegrees.popleft()
-            out.append(curr)
-            
-            neighbors = graph.get(curr, set())
-            
-            for neighbor in neighbors:
-                inDegrees[neighbor] -= 1
-                if inDegrees[neighbor] == 0:
-                    zeroInDegrees.append(neighbor)
-        
-        # not all letters are in the output, there was a cycle and no valid ordering
-        if len(out) < len(inDegrees):
+        graph = self.build_graph(words)
+        if graph == -1:
             return ''
         
-        return ''.join(out)
-                
-            
+        nodes = set([char for word in words for char in word])
         
-                 
+        # find the node that we start with -> nodes with 0 indegrees
+        indegrees, zeroInDegrees = self.get_zero_indegrees(nodes, graph)
+        
+        sortedOrder = []
+        while zeroInDegrees:
+            curr = zeroInDegrees.pop()
+            sortedOrder.append(curr)
             
+            for neighbor in graph[curr]:
+                indegrees[neighbor] -= 1
+                if indegrees[neighbor] == 0:
+                    zeroInDegrees.append(neighbor)
+        
+        if len(sortedOrder) < len(nodes):
+            return ""
+        
+        return ''.join(sortedOrder)
