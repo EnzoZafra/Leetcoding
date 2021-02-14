fa7ed7e6f4c6799ef01a4d6b9714fc3a8f8da52d
Enzo Zafra <zafra@ualberta.ca>
2021-02-14 14:32:53 -0700

    Time: 28 ms (68.06%), Space: 13.7 MB (71.50%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,73 +1,59 @@
 class Solution(object):
+    def find_pivot(self, nums):
+        # to find the index where the sorted array is rotated
+        
+        start = 0
+        end = len(nums) - 1
+
+        if nums[start] < nums[end]:
+            return 0
+        
+        while start <= end:
+            mid = (end + start) // 2
+            
+            # pivot is somewhere on the right side
+            if nums[mid] > nums[mid + 1]:
+                return mid + 1 
+            else:
+                if nums[mid] >= nums[start]:
+                    start = mid + 1 
+                else:
+                    end = mid - 1
+        
+        return 0
+    
+    def binary_search(self, nums, target, start, end):
+        while start <= end:
+            
+            mid = (end + start) // 2
+            if nums[mid] > target:
+                end = mid - 1
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                return mid
+            
+        return -1 
+                    
     def search(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        def find_pivot(left, right):
-            if nums[left] < nums[right]:
-                return 0
-
-            while left <= right:
-                mid = (left + right) // 2
-
-                # if not sorted, we found the pivot
-                if nums[mid+1] < nums[mid]:
-                    return mid + 1
-                else:
-                    # check if the first item is still smaller than mid
-                    if nums[left] <= nums[mid]:
-                        left = mid + 1 
-                    else:
-                        right = mid - 1
-                        
-            return -1
-                
-
-        
-        def bst(start, end):
-            while start <= end:
-                mid = (end + start) // 2
-
-                # target is lower than mid number
-                if nums[mid] > target:
-                    end = mid - 1
-                elif nums[mid] < target:
-                    start = mid + 1
-                else:
-                    return mid
-
-            return -1
-        
-        if not nums:
-            return -1
-        
         if len(nums) == 1:
-            return 0 if target == nums[0] else -1
+            return 0 if nums[0] == target else -1
         
-        # bst to find the pivot point
-        pivot = find_pivot(0, len(nums) - 1)
+        pivot = self.find_pivot(nums)
         print(pivot)
         
         if nums[pivot] == target:
             return pivot
-        
-        # if not rotated, BST all
+        # not rotated, normal binary search
         if pivot == 0:
-            return bst(0, len(nums) - 1)
+            return self.binary_search(nums, target, 0, len(nums) - 1)
         
-        # if target is greater than the first item, that means search the left half
-        if target > nums[0]:
-            return bst(0, pivot)
-        elif target < nums[0]:
-            return bst(pivot, len(nums)-1)
-        else:
-            return 0
-        
-        return -1
-        
-        
-        
-        
-        
+        # target is bigger than first number, then the target is somewhere right side
+        if target < nums[0]:
+            return self.binary_search(nums, target, pivot, len(nums) - 1)
+        return self.binary_search(nums, target, 0, pivot) 
