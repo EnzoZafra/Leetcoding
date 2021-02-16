7640e552c9dba908409ecb9975a5e33dc98cb371
Enzo Zafra <zafra@ualberta.ca>
2021-02-16 13:20:37 -0700

    Time: 172 ms (32.22%), Space: 16.9 MB (41.88%) - LeetHub

--- edit-distance/edit-distance.py
+++ edit-distance/edit-distance.py
@@ -1,58 +1,38 @@
 class Solution(object):
-    def editDistance(self, word1, word2, m, n):
-        # if word1 is an empty string, return the length of word2 because we need to insert that many
-        if m == 0:
-            return n
-        # if word2 is an empty string, return the length of word 1 beause we need to remove that many 
-        if n == 0:
-            return m
-        
-        # if the last characters of each are the same, recur
-        if word1[m-1] == word2[n-1]:
-            return self.editDistance(word1, word2, m-1, n-1)
-        else:
-            insert = self.editDistance(word1, word2, m, n-1) # inserting into word1 is like removing a char from word2
-            remove = self.editDistance(word1, word2, m-1, n) # removing a char from word1
-            replace = self.editDistance(word1, word2, m-1, n-1) # replacing 
-            return 1 + min(insert, remove, replace)
-            
-        
-        
     def minDistance(self, word1, word2):
         """
         :type word1: str
         :type word2: str
         :rtype: int
         """
-        if not word1:
-            return len(word2)
-        if not word2:
-            return len(word1)
-
-        R = len(word1) + 1
-        C = len(word2) + 1
         
-        dp = [[0 for _ in range(C)] for _ in range(R)]
+        word1len = len(word1)
+        word2len = len(word2)
         
-        # fill base case
-        for i in range(R):
-            dp[i][0] = i
+        if word1len == 0:
+            return word2len
         
-        for i in range(C):
-            dp[0][i] = i
-            
-        for row in range(1,R):
-            for col in range(1,C):
-                # check if the characters are the same
+        if word2len == 0:
+            return word1len
+        
+        dp = [[0 for _ in range(word2len+1)] for _ in range(word1len+1)]
+        
+        for row in range(word1len+1):
+            dp[row][0] = row
+        
+        for col in range(word2len+1):
+            dp[0][col] = col
+        
+        for row in range(1, word1len+1):
+            for col in range(1, word2len+1):
                 if word1[row-1] == word2[col-1]:
-                    # then its just the same distance as the last subproblem (diagonally)
                     dp[row][col] = dp[row-1][col-1]
+                
                 else:
-                    add_char = dp[row][col-1]
-                    remove_char = dp[row-1][col]
-                    replace_char = dp[row-1][col-1]
-                    dp[row][col] = 1 + min(add_char, remove_char, replace_char)
-        print(dp)
-        return dp[R-1][C-1]
+                    removeFromWord1 = dp[row-1][col]
+                    addToWord1 = dp[row][col-1]
+                    replaceAChar = dp[row-1][col-1]
                     
-        
+                    dp[row][col] = min(removeFromWord1, addToWord1, replaceAChar) + 1
+       
+        return dp[-1][-1]
