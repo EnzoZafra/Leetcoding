484cc2b43f207460cb6e88ac8ad62c3685b2bc1e
Enzo Zafra <zafra@ualberta.ca>
2021-02-17 14:56:57 -0700

    Time: 44 ms (66.95%), Space: 13.7 MB (59.35%) - LeetHub

--- word-search-ii/word-search-ii.py
+++ word-search-ii/word-search-ii.py
@@ -1,43 +1,59 @@
 class Solution(object):
+    def findStartingLetters(self, words):
+        startingLetterMap = collections.defaultdict(list)
+        
+        for word in words:
+            startingLetterMap[word[0]].append(word)
+        
+        return startingLetterMap
+    
+    def isValidMove(self, row, col):
+        return 0 <= row < self.rowLen and 0 <= col < self.colLen
+
+    def dfs(self, board, word, row, col):
+        start = ((row, col, 0)) 
+        stack = [start]
+        visited = set()
+        
+        while stack:
+            currRow, currCol, currIndex = stack.pop()
+            visited.add((currRow, currCol))
+            
+            if currIndex == len(word)-1:
+                return True
+            
+            for move in self.moves:
+                dr = currRow + move[0]
+                dc = currCol + move[1]
+                
+                if self.isValidMove(dr, dc) and (dr,dc) not in visited and currIndex+1 < len(word) and board[dr][dc] == word[currIndex+1]:
+                    stack.append((dr, dc, currIndex+1))
+        
+        return False
+                
+    
     def findWords(self, board, words):
         """
         :type board: List[List[str]]
         :type words: List[str]
         :rtype: List[str]
         """
-        starter = collections.defaultdict(list)
-        for word in words:
-            starter[word[0]].append(word)
+        if not board or not words:
+            return []
         
-        R = len(board)
-        C = len(board[0])
-        moves = [(0,1), (1, 0), (0, -1), (-1, 0)]
         out = set()
+        self.moves = [(1,0), (0,1), (-1,0), (0,-1)]
+        startingLetterMap = self.findStartingLetters(words)
         
-        for row in range(R):
-            for col in range(C):
-                char = board[row][col]
-                
-                if char in starter:
-                    possibleWords = starter[char]
-                    for temp in possibleWords:
-                        # DFS
-                        stack = []
-                        possibleWord = temp[1:]
-                        stack.append((row, col, temp[1:]))
-                        visited = set()
-                        while stack:
-                            currRow, currCol, possibleWord = stack.pop() 
-                            visited.add((currRow, currCol))
-                            if possibleWord == '':
-                                out.add(temp)
-                                break
-                                
-                            for move in moves:
-                                dr = currRow + move[0]
-                                dc = currCol + move[1]
-
-                                if dr >= 0 and dc >= 0 and dr < R and dc < C and (dr, dc) not in visited and board[dr][dc] == possibleWord[0]:
-                                    stack.append((dr, dc, possibleWord[1:]))
-                                    
+        self.rowLen = len(board)
+        self.colLen = len(board[0])
+        
+        for row in range(self.rowLen):
+            for col in range(self.colLen):
+                if board[row][col] in startingLetterMap:
+                    for word in startingLetterMap[board[row][col]]:
+                        # dfs and see if we can get the word
+                        if self.dfs(board, word, row, col):
+                            out.add(word)
+        
         return out
