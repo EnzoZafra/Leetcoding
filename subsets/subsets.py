241fe96ef3215554f32c38b87e3567d4698860f7
Enzo Zafra <zafra@ualberta.ca>
2021-02-17 16:12:28 -0700

    Time: 12 ms (99.22%), Space: 13.8 MB (19.26%) - LeetHub

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -4,25 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        def backtrack(subset, currIndex):
-            # append subset to our output
-            # since python lists are passed by reference, we need to append a copy
-            superset.append(subset[:])
-
-            # 1. Choose from a list of options and add it to our subset
-            for i in range(currIndex, len(nums)):
-                subset.append(nums[i])
-
-                # 2. This question does not have constraints
-
-                # 3. Recurse
-                backtrack(subset, i+1)
-
-                # 4. Remove the choice from step 1
-                subset.pop()
+        out = [] 
+        def helper(arr, start):
+            out.append(arr[:])
+            
+            for i in range(start, len(nums)):
+                arr.append(nums[i])
+                helper(arr, i+1)
+                arr.pop()
         
-        superset = []
-        backtrack([], 0)
-        nums.sort()
-        
-        return superset
+        helper([], 0)
+        return out
+                
