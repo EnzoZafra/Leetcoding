7c00601f42c7173230bc7f8bd9b24fa1355514d2
Enzo Zafra <zafra@ualberta.ca>
2021-02-18 11:07:14 -0700

    Time: 1072 ms (30.43%), Space: 22.1 MB (37.93%) - LeetHub

--- wildcard-matching/wildcard-matching.py
+++ wildcard-matching/wildcard-matching.py
@@ -8,6 +8,7 @@ class Solution(object):
         s_len = len(s)
         p_len = len(p)
         
+        # if the pattern is empty, its only true if the string is also empty
         if p == '':
             return True if s == '' else False
         
@@ -24,9 +25,15 @@ class Solution(object):
         for p_index in range(1, p_len+1):
             for s_index in range(1, s_len+1):
                 
+                # if we see a '*', there are two cases:
+                # 1. ignore the * and move to the next character in pattern (* indicates empty sequence)
+                # 2. * character matches with the s_index character in the input
                 if p[p_index - 1] == '*':
-                    dp[p_index][s_index] = dp[p_index-1][s_index] or dp[p_index][s_index-1]
+                    dp[p_index][s_index] = dp[p_index][s_index-1] or dp[p_index-1][s_index]
                 
+                # considered as matching if:
+                # 1. current character in pattern is ?
+                # 2. characters actually match
                 elif p[p_index - 1] == '?' or p[p_index-1] == s[s_index-1]:
                     dp[p_index][s_index] = dp[p_index-1][s_index-1]
                 
