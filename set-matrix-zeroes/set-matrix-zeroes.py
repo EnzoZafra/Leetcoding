81817f06f8d6fea16e3ac7268c444c2c3129d36d
Enzo Zafra <zafra@ualberta.ca>
2021-02-27 16:27:58 -0700

    Time: 108 ms (78.23%), Space: 14.5 MB (27.15%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.py
+++ set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,19 +4,25 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        rowLen = len(matrix)
+        colLen = len(matrix[0])
+        
+        zero_cols = set()
         zero_rows = set()
-        zero_columns = set()
         
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
+        for row in range(rowLen):
+            for col in range(colLen):
                 if matrix[row][col] == 0:
+                    zero_cols.add(col)
                     zero_rows.add(row)
-                    zero_columns.add(col)
-        
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                if row in zero_rows or col in zero_columns:
+                    
+        for row in range(rowLen):
+            if row in zero_rows:
+                matrix[row] = [0 for _ in range(colLen)]
+                continue
+                
+            for col in range(colLen):
+                if col in zero_cols:
                     matrix[row][col] = 0
         
         return matrix
-                    
