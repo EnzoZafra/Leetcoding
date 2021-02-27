b2aaf456a7eff202153980b8fc4ce10b580a044f
Enzo Zafra <zafra@ualberta.ca>
2021-02-27 15:27:02 -0700

    Time: 12 ms (95.06%), Space: 13.5 MB (40.56%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -4,18 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
-        # if n = 3
-        # _ _ _
-        #          
-        if n == 1:
-            return 1
+        if n <= 2:
+            return n
         
         dp = [0 for _ in range(n+1)]
         dp[1] = 1
         dp[2] = 2
         
         for i in range(3, n+1):
-            dp[i] = dp[i-2] + dp[i-1]
+            dp[i] = dp[i-1] + dp[i-2]
         
         return dp[-1]
