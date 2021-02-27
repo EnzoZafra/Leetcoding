9169a9714d7797fa060a0b4f2afd64ab4120f131
Enzo Zafra <zafra@ualberta.ca>
2021-02-27 15:59:59 -0700

    Time: 56 ms (98.95%), Space: 32.5 MB (46.17%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.py
+++ palindrome-linked-list/palindrome-linked-list.py
@@ -1,40 +1,61 @@
-class Solution:
-    def isPalindrome(self, head):
-        if head is None:
-            return True
-
-        # Find the end of first half and reverse second half.
-        first_half_end = self.end_of_first_half(head)
-        second_half_start = self.reverse_list(first_half_end.next)
-
-        # Check whether or not there's a palindrome.
-        result = True
-        first_position = head
-        second_position = second_half_start
-        while result and second_position is not None:
-            if first_position.val != second_position.val:
-                result = False
-            first_position = first_position.next
-            second_position = second_position.next
-
-        # Restore the list and return the result.
-        first_half_end.next = self.reverse_list(second_half_start)
-        return result    
-
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
     def end_of_first_half(self, head):
         fast = head
         slow = head
-        while fast.next is not None and fast.next.next is not None:
+        while fast.next and fast.next.next:
             fast = fast.next.next
             slow = slow.next
+            
         return slow
-
-    def reverse_list(self, head):
-        previous = None
-        current = head
-        while current is not None:
-            next_node = current.next
-            current.next = previous
-            previous = current
-            current = next_node
-        return previous
+    
+    def reverse_list(self, node):
+        prev = None
+        curr = node
+        
+        while curr:
+            temp_next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp_next
+        
+        return prev
+    
+    def restore_list(self, first_half_end, second_half_start):
+        # to restore, we have to reverse the list again
+        
+        first_half_end.next = self.reverse_list(second_half_start)
+    
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        
+        if not head:
+            return True
+        
+        first_half_end = self.end_of_first_half(head)
+        second_half_start = self.reverse_list(first_half_end.next)
+        
+        # check if there's a palindrome
+        first = head
+        second = second_half_start
+        
+        # keep a variable for result so that the linkedlist can be restored
+        while second:
+            if first.val != second.val:
+                # restore the list
+                self.restore_list(first_half_end, second_half_start)
+                return False
+            
+            first = first.next
+            second = second.next
+            
+        self.restore_list(first_half_end, second_half_start)
+        return True
+        
