770711366599fb24e459aaafbf6ef36ca2c90e78
Enzo Zafra <zafra@ualberta.ca>
2021-02-26 18:14:41 -0700

    Time: 572 ms (75.25%), Space: 13.5 MB (99.10%) - LeetHub

--- shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,41 +1,33 @@
 class Solution(object):
     def shortestPathBinaryMatrix(self, grid):
-        
-        max_row = len(grid) - 1
-        max_col = len(grid[0]) - 1
-        directions = [
-            (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        
-        # Helper function to find the neighbors of a given cell.
-        def get_neighbours(row, col):
-            for row_difference, col_difference in directions:
-                new_row = row + row_difference
-                new_col = col + col_difference
-                if not(0 <= new_row <= max_row and 0 <= new_col <= max_col):
-                    continue
-                if grid[new_row][new_col] != 0:
-                    continue
-                yield (new_row, new_col)
-        
-        # Check that the first and last cells are open. 
-        if grid[0][0] != 0 or grid[max_row][max_col] != 0:
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        if grid[0][0] == 1 or grid[-1][-1] == 1: 
             return -1
         
-        # Set up the BFS.
-        queue = deque()
-        queue.append((0, 0))
-        grid[0][0] = 1 
+        rowLen = len(grid)
+        colLen = len(grid[0])
         
-        # Carry out the BFS.
+        moves = [(0,1), (1,0), (0,-1), (-1,0), (1,1), (-1,1), (1,-1), (-1,-1)]
+        
+        queue = deque() 
+        queue.append((0,0,1))
+        grid[0][0] = 1
+        
         while queue:
-            row, col = queue.popleft()
-            distance = grid[row][col]
-            if (row, col) == (max_row, max_col):
-                return distance
-            for neighbour_row, neighbour_col in get_neighbours(row, col):
-                grid[neighbour_row][neighbour_col] = distance + 1
-                queue.append((neighbour_row, neighbour_col))
+            currRow, currCol, count = queue.popleft()
+            if currRow == colLen - 1 and currCol == rowLen - 1:
+                return count
+            
+            for move in moves:
+                dr = currRow + move[0]
+                dc = currCol + move[1]
+                
+                if 0 <= dr < rowLen and 0 <= dc < colLen and grid[dr][dc] == 0:
+                    grid[dr][dc] = 1
+                    queue.append((dr, dc, count+1))
         
-        # There was no path.
         return -1
-        
+    
