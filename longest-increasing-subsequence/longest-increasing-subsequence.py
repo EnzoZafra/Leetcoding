b88abc278729e7a612cc57afe5f1826e468dd623
Enzo Zafra <zafra@ualberta.ca>
2021-02-27 21:21:38 -0700

    Time: 4812 ms (5.67%), Space: 13.9 MB (17.25%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.py
+++ longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -4,19 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        dp = [1 for _ in range(n)]
-        out = 1
+        dp = [1 for _ in range(len(nums))]
+        maxSoFar = 1
         
-        for i in range(1, n):
-            curr_max = 0
+        for i in range(len(nums)):
             for j in range(i):
                 if nums[j] < nums[i]:
-                    # this would always get the best longest increasing subsequence so far
-                    curr_max = max(curr_max, dp[j])
+                    dp[i] = max(dp[i], dp[j]+1)
+                    maxSoFar = max(maxSoFar, dp[i])
+        return maxSoFar
             
-            # this would be 1 if there is no number nums[j] 0 <= j < i smaller than i 
-            dp[i] = curr_max + 1
-            out = max(out, dp[i])
-        
-        return out
