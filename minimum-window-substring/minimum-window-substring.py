959b3b36bd9f658b11452f699658585338339977
Enzo Zafra <zafra@ualberta.ca>
2021-02-28 15:03:23 -0700

    Time: 136 ms (41.06%), Space: 14 MB (97.52%) - LeetHub

--- minimum-window-substring/minimum-window-substring.py
+++ minimum-window-substring/minimum-window-substring.py
@@ -5,45 +5,37 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        # sliding window
-        # grow window by moving end pointer to the right until we meet the constraint
-        # keep growing until we break the constraint
-        
-        # compress the window by moving the start pointer to the right until we meet the constraint again
-        
-        if not s or not t:
-            return ''
-        
         occ = Counter(t)
-        counter = len(occ) 
-
+        count = len(occ)
+        
         start = 0
         end = 0
-        minWindow = '' 
-        minWindowLength = float('inf')
+        
+        out = ''
+        minWindowSize = float('inf')
         
         while end < len(s):
-            endChar = s[end] 
+            endChar = s[end]
             
             if endChar in occ:
                 occ[endChar] -= 1
                 if occ[endChar] == 0:
-                    counter -= 1
-                    
-            while counter == 0:
-                if (end - start + 1 < minWindowLength):
-                    minWindowLength = end - start + 1
-                    minWindow = s[start:end+1]
-                    
+                    count -= 1
+            
+            # start shrinking window
+            while count == 0:
+                if end - start + 1 < minWindowSize:
+                    minWindowSize = end - start + 1
+                    out = s[start:end+1]
+                
                 startChar = s[start]
                 if startChar in occ:
                     if occ[startChar] == 0:
-                        counter += 1
+                        count += 1
+                        
                     occ[startChar] += 1
                     
                 start += 1
             end += 1
-
-        return minWindow
-                
-            
+        
+        return out
