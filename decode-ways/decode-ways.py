a9bdf0da371e6661e5a83231a14067a6595be902
Enzo Zafra <zafra@ualberta.ca>
2021-03-01 12:49:01 -0700

    Time: 16 ms (90.56%), Space: 13.5 MB (80.43%) - LeetHub

---
+++ decode-ways/decode-ways.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dp = [0 for _ in range(len(s) + 1)]
+        
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
+        
+        for i in range(2, len(s)+1):
+            
+            if s[i-1] != '0':
+                dp[i] = dp[i-1]
+            
+            # check two digits
+            two_digit = int(s[i-2:i])
+            if 10 <= two_digit <= 26:
+                dp[i] += dp[i-2]
+        
+        return dp[-1]
