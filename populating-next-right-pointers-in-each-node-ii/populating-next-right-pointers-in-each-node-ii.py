b084add14937b27d90abafd40b11da9cfa8f2a03
Enzo Zafra <zafra@ualberta.ca>
2021-02-28 18:19:21 -0700

    Time: 80 ms (8.08%), Space: 15.4 MB (54.27%) - LeetHub

---
+++ populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,47 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+import collections 
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if not root:
+            return root
+        
+        # Initialize a queue data structure which contains
+        # just the root of the tree
+        Q = collections.deque([root])
+        
+        # Outer while loop which iterates over 
+        # each level
+        while Q:
+            
+            # Note the size of the queue
+            size = len(Q)
+            
+            # Iterate over all the nodes on the current level
+            for i in range(size):
+                
+                # Pop a node from the front of the queue
+                node = Q.popleft()
+                
+                if i < size - 1:
+                    node.next = Q[0]
+                
+                # Add the children, if any, to the back of
+                # the queue
+                if node.left:
+                    Q.append(node.left)
+                if node.right:
+                    Q.append(node.right)
+        
+        # Since the tree has now been modified, return the root node
+        return root
