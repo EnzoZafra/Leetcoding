cf33e6aebfb452e20ff7082b3766a508a8d28d14
Enzo Zafra <zafra@ualberta.ca>
2021-03-01 13:26:13 -0700

    Time: 56 ms (88.87%), Space: 16.6 MB (98.04%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.py
+++ meeting-rooms-ii/meeting-rooms-ii.py
@@ -4,28 +4,18 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        starts = []
-        ends = []
-        for interval in intervals:
-            starts.append(interval[0])
-            ends.append(interval[1])
+        starts = [x[0] for x in intervals]
+        ends = [x[1] for x in intervals]
         
         starts.sort()
         ends.sort()
         
+        currEnd = 0
         rooms = 0
+        for start in starts:
+            if start < ends[currEnd]:
+                rooms += 1 
+            else:
+                currEnd += 1
         
-        endPtr = 0
-        startPtr = 0
-        
-        while startPtr < len(intervals):
-            if starts[startPtr] >= ends[endPtr]:
-                rooms -= 1
-                endPtr += 1
-                
-            rooms += 1
-            startPtr += 1
-       
         return rooms
-             
-        
