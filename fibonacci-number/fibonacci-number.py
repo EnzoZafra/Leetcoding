e94b4d518629753326692be3782f9856771a42fb
Enzo Zafra <zafra@ualberta.ca>
2021-03-02 19:01:29 -0700

    Time: 32 ms (66.52%), Space: 14.5 MB (9.96%) - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -1,17 +1,12 @@
-class Solution(object):
-    def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n == 0:
-            return 0
-        
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+
         dp = [0 for _ in range(n+1)]
         dp[1] = 1
         
         for i in range(2, n+1):
             dp[i] = dp[i-1] + dp[i-2]
         
-        print(dp)
         return dp[-1]
