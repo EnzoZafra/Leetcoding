90974c01797bd42a53695ba8307ea4fd930bd789
Enzo Zafra <zafra@ualberta.ca>
2021-03-03 22:42:54 -0700

    Time: 40 ms (55.52%), Space: 14.7 MB (60.81%) - LeetHub

--- clone-graph/clone-graph.py
+++ clone-graph/clone-graph.py
@@ -1,37 +1,55 @@
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution(object):
-    def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        
+        def generate_copies(node):
+            copyMap = {}
+
+            stack = [node]
+            visited = set(stack)
+            while stack:
+                curr = stack.pop()
+                
+                copy = Node(curr.val)
+                copyMap[curr.val] = copy
+
+                for neighbor in curr.neighbors:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        stack.append(neighbor)
+            
+            return copyMap
+        
+        def populate_neighbors(node, copyMap):
+            stack = [node]
+            visited = set(stack)
+            while stack:
+                curr = stack.pop()
+                
+                copy = copyMap[curr.val]
+                
+                for neighbor in curr.neighbors:
+                    neighborCopy = copyMap[neighbor.val]
+                    copy.neighbors.append(neighborCopy)
+                    
+                    if neighbor not in visited: 
+                        visited.add(neighbor)
+                        stack.append(neighbor)
+            
+            return copyMap[node.val]
+        
         if not node:
             return None
         
-        root_clone = Node(node.val)
-
-        queue = deque()
-        queue.append(node)
-        
-        visited = {}
-        visited[node] = root_clone
-        while queue:
-            curr = queue.popleft()
-            
-            for neighbor in curr.neighbors:
-                if neighbor not in visited:
-                    cloneNeighbor = Node(neighbor.val)
-                    visited[neighbor] = cloneNeighbor
-                    queue.append(neighbor)
+        copyMap = generate_copies(node)
+        copyRoot = populate_neighbors(node, copyMap)
+        return copyRoot
                     
-                visited[curr].neighbors.append(visited[neighbor])
-                
-        return visited[node]
-                
+            
