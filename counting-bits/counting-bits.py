e11bab5140971190fca04c5c96b9143b398a980d
Enzo Zafra <zafra@ualberta.ca>
2021-03-05 21:18:42 -0700

    Time: 224 ms (7.65%), Space: 20.8 MB (78.18%) - LeetHub

--- counting-bits/counting-bits.py
+++ counting-bits/counting-bits.py
@@ -1,20 +1,18 @@
-class Solution(object):
-    def countBit(self, num):
+class Solution:
+    def countOnes(self, num):
         count = 0
         while num > 0:
-            count += num & 1
+            if num & 1:
+                count += 1
+            
             num = num >> 1
         
         return count
         
-    def countBits(self, num):
-        """
-        :type num: int
-        :rtype: List[int]
-        """
+    def countBits(self, num: int) -> List[int]:
         out = []
+        
         for i in range(num+1):
-            out.append(self.countBit(i))
+            out.append(self.countOnes(i)) 
         
         return out
-        
