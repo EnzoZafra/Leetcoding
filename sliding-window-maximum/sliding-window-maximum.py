1ed1cde56388b9ba649a9a08e59b4b1e42b20d5b
Enzo Zafra <zafra@ualberta.ca>
2021-03-09 22:29:21 -0700

    Time: 1972 ms (20.80%), Space: 30 MB (68.22%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.py
+++ sliding-window-maximum/sliding-window-maximum.py
@@ -1,79 +1,51 @@
-class Solution(object):
-    def maxSlidingWindow(self, nums, k):
-        n = len(nums)
-        if n * k == 0:
-            return []
-        
-        if k == 1:
-            return nums
-        
-        def clean_deque(i):
-            # remove indexes of elements that are not in the sliding window
-            if deq and deq[0] == i - k:
-                deq.popleft()
-            
-            # remove from deq indexes of all elements
-            # that are smaller than current element nums[i]
-            while deq and nums[i] > nums[deq[-1]]:
-                deq.pop()
-        
-        deq = deque()
-        max_idx = 0
-        for i in range(k):
-            clean_deque(i)
-            deq.append(i)
-            
-            # compute max in nums[:k]
-            if nums[i] > nums[max_idx]:
-                max_idx = i
-                
-        output = [nums[max_idx]]
-        
-        for i in range(k,n):
-            clean_deque(i)
-            deq.append(i)
-            output.append(nums[deq[0]])
-        
-        return output
-        
-        
-    def maxSlidingWindowTle(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        n = len(nums)
-        if n * k == 0:
-            return []
-        
-        if k == 1:
-            return nums
-        
-        start = 0
-        end = 0
-        
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         out = []
         
-        currMax = float('-inf')
-        while end < k:
-            currMax = max(currMax, nums[end]) 
-            end += 1
+        start = 0
+        end = min(start + k, len(nums))
         
-        out.append(currMax)
-        
-        while end < len(nums):
-            new_num = nums[end]
-            removed_num = nums[start]
+        while end < len(nums)+1:
+            window = nums[start:end]
+            out.append(max(window))
             
             start += 1
             end += 1
-            
-            if removed_num == currMax:
-                currMax = max(nums[start:end])
-            else:
-                currMax = max(currMax, new_num)
-            
-            out.append(currMax)
-            
+        
         return out
+        
+        
+    def maxSlidingWindow(self, nums: 'List[int]', k: 'int') -> 'List[int]':
+        n = len(nums)
+        if n * k == 0:
+            return []
+        if k == 1:
+            return nums
+        
+        left = [0] * n
+        left[0] = nums[0]
+        right = [0] * n
+        right[n - 1] = nums[n - 1]
+        
+        for i in range(1, n):
+            
+            # from left to right
+            if i % k == 0:
+                # block start
+                left[i] = nums[i]
+            else:
+                left[i] = max(left[i - 1], nums[i])
+                
+            # from right to left
+            j = n - i - 1
+            if (j + 1) % k == 0:
+                # block end
+                right[j] = nums[j]
+            else:
+                right[j] = max(right[j + 1], nums[j])
+        
+        output = []
+        for i in range(n - k + 1):
+            output.append(max(left[i + k - 1], right[i]))
+            
+        return output
