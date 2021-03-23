5e0974df18b990e482fa6e812615e04b9a4fa46a
Enzo Zafra <zafra@ualberta.ca>
2021-03-23 14:14:42 -0600

    Time: 40 ms (52.33%), Space: 14.6 MB (54.78%) - LeetHub

--- all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -1,58 +1,48 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def getParents(self, root):
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, K: int) -> List[int]:
         parents = {}
-        parent = None
         
-        queue = deque()
-        queue.append((root, None))
-        while queue:
-            curr, parent = queue.popleft() 
-            parents[curr] = parent
+        stack = [(root, None)]
+        while stack:
+            currNode, parent = stack.pop() 
+            parents[currNode] = parent
             
-            if curr.left:
-                queue.append((curr.left, curr))
+            if currNode.left:
+                stack.append((currNode.left, currNode))
                 
-            if curr.right:
-                queue.append((curr.right, curr))
+            if currNode.right:
+                stack.append((currNode.right, currNode))
         
-        return parents
         
-    def distanceK(self, root, target, K):
-        """
-        :type root: TreeNode
-        :type target: TreeNode
-        :type K: int
-        :rtype: List[int]
-        """
-        parents = self.getParents(root)
-        
-        # bfs from target 2 levels
+            
         queue = deque()
         queue.append((target, 0))
-        out = [] 
-        visited = set()
+        
+        out = []
+        visited = set([target])
         while queue:
-            curr, level = queue.popleft()
+            curr, dist = queue.popleft()
             visited.add(curr)
-            if level == K:
-                out.append(curr.val)  
             
+            if dist == K:
+                out.append(curr.val)
+            elif dist > K:
+                return out
+        
             if curr.left and curr.left not in visited:
-                queue.append((curr.left, level+1))
+                queue.append((curr.left, dist+1))
                 
             if curr.right and curr.right not in visited:
-                queue.append((curr.right, level+1))
+                queue.append((curr.right, dist+1))
                 
             if parents[curr] and parents[curr] not in visited:
-                queue.append((parents[curr], level+1))
-                
+                queue.append((parents[curr], dist+1))
+        
         return out
-        
-        
